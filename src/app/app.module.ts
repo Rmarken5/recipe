e2ccdd9ad0cb81f307a5c561242030d4506570d1
Ryan Marken <marken.ryan@gmail.com>
2018-11-04 11:19:44 -0500

    Application setup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
+import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
+import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './header/recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './header/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
